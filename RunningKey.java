e0b9e137ee9c12b41cdfe2fc19700c0eb29ef62c
CarsonCraig <carsonkcraig@gmail.com>
2016-01-20 17:46:42 -0500

    New Ciphers

---
+++ RunningKey.java
@@ -0,0 +1,120 @@
+
+import java.util.Scanner;
+
+/**
+ * @author carson
+ */
+
+public class RunningKey {
+
+    static Scanner in;
+    static String runningKey = "";
+    static String lengthenedRunningKey = "";
+    static int[] numericRunningKey;
+
+    static String input = "";
+    static int[] numericInput;
+
+    static String output = "";
+
+    static Boolean performEncrypt;
+
+    static int[] addedNumeric;
+
+    public static void main(String[] args) {
+        in = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Encrypt[E] or Decrypt[D]");
+            String entered = in.nextLine();
+
+            if ("E".equals(entered)) {
+                performEncrypt = true;
+                System.out.println("Enter a string to be encrypted");
+                break;
+            } else if ("D".equals(entered)) {
+                performEncrypt = false;
+                System.out.println("Enter a string to be decrypted");
+                break;
+            } else {
+                System.out.println("Invalid Input");
+            }
+        }
+
+        input = in.nextLine();
+        input = formatString(input);
+        numericInput = turnNumeric(input);
+
+        System.out.println("Enter a running key longer than the " + (performEncrypt ? "plaintext." : "ciphertext."));
+        runningKey = in.nextLine();
+        runningKey = formatString(runningKey);
+
+        lengthenedRunningKey = generateRunningKey(runningKey, input);
+
+
+        numericRunningKey = turnNumeric(lengthenedRunningKey);
+
+        addedNumeric = addNumericStrings(numericInput, numericRunningKey);
+        output = turnAlpha(addedNumeric);
+        System.out.println(output + "");
+    }
+
+    public static String formatString(String originalString) {
+        String formattedString = "";
+
+        formattedString = (originalString.replaceAll("[^A-Za-z]", "")).toUpperCase();
+
+        return formattedString;
+    }
+
+    public static String generateRunningKey(String runningKey, String input) {
+        String returnRunningKey = "";
+
+        while (returnRunningKey.length() < input.length()) {
+            returnRunningKey = returnRunningKey + runningKey;
+        }
+
+        returnRunningKey = returnRunningKey.substring(0, input.length());
+
+        return returnRunningKey;
+    }
+
+
+    public static int[] turnNumeric(String originalString) {
+        int[] returnNumeric = new int[originalString.length()];
+
+        for (int i = 0; i < originalString.length(); i++) {
+
+            returnNumeric[i] = ((int) originalString.charAt(i)) - 65;
+        }
+
+        return returnNumeric;
+    }
+
+    public static int[] addNumericStrings(int[] numericInput, int[] numericRunningKey) {
+
+        int[] returnAddedNumeric = new int[numericInput.length];
+
+        for (int i = 0; i < numericInput.length; i++) {
+            if (performEncrypt) {
+                returnAddedNumeric[i] = (numericInput[i] + numericRunningKey[i]) % 26;
+            } else {
+                returnAddedNumeric[i] = (numericInput[i] + (26 - numericRunningKey[i])) % 26;
+            }
+        }
+
+        return returnAddedNumeric;
+    }
+
+    public static String turnAlpha(int[] addedNumeric) {
+        String returnString = "";
+
+        for (int i = 0; i < addedNumeric.length; i++) {
+
+            returnString = returnString + (char) (addedNumeric[i] + 65);
+
+        }
+
+        return returnString;
+    }
+}
